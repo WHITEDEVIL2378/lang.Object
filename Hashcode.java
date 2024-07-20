2ea26ae6d460b2152ca638d43b7cb5c09083d8dc
WHITEDEVIL2378 <haribala2378@gmail.com>
2024-07-20 10:56:59 +0530

    changed

--- Hashcode.java
+++ Hashcode.java
@@ -5,7 +5,15 @@ class Hashcode
 	{
 		this.i = i;
 	}
-	public static void main(String args[])
+	public String toString()     //our class toString() method called so hashCode() method not called
+	{
+		return i + "";
+	}
+	public int hashCode()               //object class toString() method called but our own class hashCode() method called
+	{
+		return i;
+	}
+	public static void main(String args[])//object class toString() method called so object class hashCode() method called
 	{
 		Hashcode s1 = new Hashcode(10);
 		Hashcode s2 = new Hashcode(100);
