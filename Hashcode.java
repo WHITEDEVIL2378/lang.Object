01d573977fefcc70ec827c792753848dcc027129
WHITEDEVIL2378 <haribala2378@gmail.com>
2024-07-20 10:43:28 +0530

    obj

---
+++ Hashcode.java
@@ -0,0 +1,20 @@
+class Hashcode
+{
+	int i;
+	Hashcode(int i)
+	{
+		this.i = i;
+	}
+	public int hashcode()
+	{
+		return i;
+	}
+	public static void main(String args[])
+	{
+		Hashcode s1 = new Hashcode(10);
+		Hashcode s2 = new Hashcode(100);
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+}
+		
