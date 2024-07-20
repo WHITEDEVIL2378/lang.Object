a5780a47ad14885ecfc70699fa5c1e71c5bdd5d6
WHITEDEVIL2378 <haribala2378@gmail.com>
2024-07-20 10:27:50 +0530

    obj

---
+++ Test.java
@@ -0,0 +1,23 @@
+class Test
+{
+	String name;
+	int rollno;
+	Test(String name,int rollno)
+	{
+		this.name = name;
+		this.rollno = rollno;
+	}
+	public String toString()
+	{
+		return name + "..." + rollno;
+	}
+	public static void main(String args[])
+	{
+		Test s1 = new Test("durga",101);
+		Test s2 = new Test("Ravi",102);
+		System.out.println(s1);
+		System.out.println(s1.toString());
+		System.out.println(s2);
+	}
+}
+		
